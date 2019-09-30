2d64aa94b084468b4ca00a7dc769efd751d0a77c
Matthew Wise <matthew.rs.wise@gmail.com>
2019-09-29 17:50:22 -0700

    removed sign up option

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations] 
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
   scope '/admin' do
     resources :users
     post '/users/:id/resetpassword', to: 'users#resetpassword', as: 'reset_user_password'
