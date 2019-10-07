7e79733ebae30777f636a02727b2bf6c444e2cd5
Matthew Wise <matthew.rs.wise@gmail.com>
2019-10-06 17:09:37 -0700

    add domain env var for sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :aws_sdk
   
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => ENV.fetch("SIMPLEBLOG_DOMAIN") }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
