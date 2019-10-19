0f02a834304671a6488b39061bd995a0a5bd2859
Matthew Wise <matthew.rs.wise@gmail.com>
2019-10-19 12:21:17 -0700

    Let users change their name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def self.get_settings
     return Setting.first || Setting.new
   end
@@ -11,4 +14,10 @@ class ApplicationController < ActionController::Base
       redirect_to posts_path
     end
   end 
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
