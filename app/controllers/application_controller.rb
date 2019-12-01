c83dd36d0b318c56275c8122efbe3705ee022e50
Matthew Wise <matthew.rs.wise@gmail.com>
2019-11-30 18:27:08 -0800

    refactor basic_auth check into application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,22 @@ class ApplicationController < ActionController::Base
     return Setting.first || Setting.new
   end
 
+  def check_basic_auth
+    basic_auth_header = request.authorization
+    if basic_auth_header.nil?
+      return false
+    end
+    credentials = Base64.decode64(basic_auth_header.split("Basic ").last)
+    basic_auth_user, basic_auth_pass = credentials.split(":")
+    if (basic_auth_user.nil? || basic_auth_pass.nil?)
+      return false
+    end
+    user = User.find_by(basic_auth_username: basic_auth_user)
+    if (user.nil? || user.basic_auth_password != basic_auth_pass)
+      return false
+    end
+    return true
+  end
 
   private
   
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,25 +11,13 @@ class PostsController < ApplicationController
   end
 
   def rss
-    basic_auth_header = request.authorization
-    if basic_auth_header.nil?
+    if !check_basic_auth
       head :unauthorized
-      return
+    else
+      @posts = Post.order(datetime: :desc).page(1)
+      @settings = SettingsController.get_setting
+      render layout: false
     end
-    credentials = Base64.decode64(basic_auth_header.split("Basic ").last)
-    basic_auth_user, basic_auth_pass = credentials.split(":")
-    if (basic_auth_user.nil? || basic_auth_pass.nil?)
-      head :unauthorized
-      return
-    end
-    user = User.find_by(basic_auth_username: basic_auth_user)
-    if (user.nil? || user.basic_auth_password != basic_auth_pass)
-      head :unauthorized
-      return
-    end
-    @posts = Post.order(datetime: :desc).page(1)
-    @settings = SettingsController.get_setting
-    render layout: false
   end
 
   def show
