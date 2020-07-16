bf0b4a138205bd393a16e95d467e5b59fec9e9cc
Matthew Wise <matthew.rs.wise@gmail.com>
2020-07-16 02:38:47 +0000

    add DNS to cleanup script

--- deploymentscripts/cleanup-aws.rb
+++ deploymentscripts/cleanup-aws.rb
@@ -7,6 +7,7 @@
 #load 'aws-helpers.rb'
 require 'aws-sdk-ec2'
 require 'aws-sdk-iam'
+require 'aws-sdk-route53'
 require 'base64'
 
 AMI_ID = "ami-06f2f779464715dc5" # Ubuntu 18.04 LTS 64bit x86
@@ -17,7 +18,7 @@ CIDR = '10.200.0.0/16'
 #NAME = "SimpleBlog"
 
 ec2 = Aws::EC2::Resource.new(region: REGION)
-
+r53 = Aws::Route53::Resource.new.client
 
 if (ARGV.length < 1)
   puts "Please specify your domain name as a parameter to this script"
@@ -26,6 +27,52 @@ end
 domain = ARGV[0] #TODO: Verify domain is owned, otherwise DNS update will fail
 raise("only 1 level subdomain allowed") if (domain.split(".").count > 3)
 NAME = domain
+
+### DNS Cleanup ###
+def delete_dns_record_set(r53_client:, hosted_zone:, domain:, ip_address:)
+  puts "Deleting record set pointing #{domain} to #{ip_address}"
+  record_set = r53_client.change_resource_record_sets({
+    change_batch: {
+      changes: [
+        {
+          action: "DELETE",
+          resource_record_set: {
+            name: domain,
+            resource_records: [
+              {
+                value: ip_address,
+              },
+            ],
+	    ttl: 300,
+            type: "A",
+          },
+        },
+      ],
+      comment: "#{domain} web server for #{domain}",
+    },
+    hosted_zone_id: hosted_zone.id,
+  })
+end
+
+def find_hosted_zone(r53_client:, domain_url:)
+  url_parts = domain_url.split(".")
+  if (url_parts.size > 3) or (url_parts.size < 2)
+    raise("invalid domain: #{domain_url}")
+  end
+  domain = "#{url_parts[-2]}.#{url_parts[-1]}"
+  hosted_zone_list = r53_client.list_hosted_zones({
+    max_items: 500
+  })
+  hosted_zone_list.hosted_zones.each do |hosted_zone|
+    if hosted_zone.name == "#{domain}."
+      puts "  Found hosted zone with ID #{hosted_zone.id} for #{domain}"
+      return hosted_zone
+    end
+  end
+  return nil
+end
+
+hosted_zone = find_hosted_zone(r53_client: r53, domain_url: domain)
 
 ### EC2 Instance ###
 ec2.instances({filters: [{name: 'tag:Name', values: ["#{NAME}Instance"]}]}).each do |i|
@@ -46,6 +93,7 @@ ec2.instances({filters: [{name: 'tag:Name', values: ["#{NAME}Instance"]}]}).each
     ec2.client.release_address({
       allocation_id: addr.allocation_id, 
     })
+    delete_dns_record_set(r53_client: r53, hosted_zone: hosted_zone, domain: domain, ip_address: addr.public_ip)
   end
 
   case i.state.code
