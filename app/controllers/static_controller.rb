f68b8f0aafc07bd66182055eb8de6f08cb27bd87
Matthew Wise <matthew.rs.wise@gmail.com>
2020-12-18 09:15:43 -0600

    Add help page for changing CSS

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,9 @@
 class StaticController < ApplicationController
   before_action :authenticate_user!
   before_action :verify_publisher
+  before_action :verify_admin, except:[:themes]
   def markdown
+  end
+  def themes
   end
 end
