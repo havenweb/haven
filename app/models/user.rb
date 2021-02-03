b75ebf2d5cc94ba7592ce032997b92a157e3fdf7
Matthew Wise <matthew.rs.wise@gmail.com>
2021-02-03 14:03:59 -0600

    fix login link dependency that breaks user delete flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
-  has_many :login_links
+  has_many :login_links, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
