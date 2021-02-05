1be9d3a9aa7f0881748719cfda5e49c3ba1f26fd
Matthew Wise <matthew.rs.wise@gmail.com>
2021-02-05 13:38:37 -0600

    allow deletion of users even if they've posted or commented

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   validates :basic_auth_username, uniqueness: true
   validates_presence_of :basic_auth_password
 
-  has_many :posts, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
+  has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :login_links, dependent: :destroy
 
   # Include default devise modules. Others available are:
