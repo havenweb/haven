07871cf5c5a5ac47833d926a37a426f0577fbc59
Matthew Wise <matthew.rs.wise@gmail.com>
2022-02-21 15:58:44 -0600

    limit size of journal log

--- deploymentscripts/lib/bash/logrotate.sh
+++ deploymentscripts/lib/bash/logrotate.sh
@@ -8,3 +8,7 @@ sudo echo "  rotate 7" >> /etc/logrotate.conf
 sudo echo "  notifempty" >> /etc/logrotate.conf
 sudo echo "  copytruncate" >> /etc/logrotate.conf
 sudo echo "}" >> /etc/logrotate.conf
+
+## This section enables size limits for the systemd journal logs
+sudo echo "SystemMaxUse=50M" >> /etc/systemd/journald.conf
+sudo service systemd-journald restart
