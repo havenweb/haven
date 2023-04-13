10172c31eabe7b190c293d54fce3e3f0b522a458
Matthew Wise <matt@havenweb.org>
2023-04-13 11:15:38 -0500

    upgrade rails to v6.0

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,12 +3,12 @@ class ImagesController < ApplicationController
   protect_from_forgery with: :exception
 
   def show
-    expires_in ActiveStorage::Blob.service.url_expires_in
+    expires_in ActiveStorage.service_urls_expire_in
     redirect_to @blob.service_url(disposition: params[:disposition])
   end
 
   def show_variant
-    expires_in ActiveStorage::Blob.service.url_expires_in
+    expires_in ActiveStorage.service_urls_expire_in
     variant = @blob.variant(combine_options:{thumbnail: "1600", quality: '65%', interlace: 'plane', auto_orient: true}).processed
     redirect_to variant.service_url(disposition: params[:disposition])
   end
--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,1 +1,1 @@
-ActiveStorage::Service.url_expires_in = 1.hour
+Rails.application.config.active_storage.service_urls_expire_in
