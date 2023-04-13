563f92ff7b5e7a2c350fb44f8cdc1301c1e62900
Matthew Wise <matt@havenweb.org>
2023-04-13 11:15:38 -0500

    upgrade rails to v6.0

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,5 @@ Rails.application.routes.draw do
   get 'login_links/validate', as: :login_link
 
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
