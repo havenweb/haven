ad94de1dfeed9ccffaffab2ac8793b84cd3fe403
Matthew Wise <matt@havenweb.org>
2023-04-27 20:12:38 -0500

    Refactor feed fetching to make it easier to test and fixes feed-fetching-related bug

---
+++ lib/haven_feed_entry.rb
@@ -0,0 +1,105 @@
+require 'cgi'
+
+class HavenFeedEntry
+  attr_reader :feed_title, :title, :link, :date, :content, :guid, :audio
+
+  ## feed is return from RSS::Parser.parse()
+  ## item is element from feed.items (rss) or feed.entries (atom)
+  def initialize(feed, item)
+    if (feed.feed_type == "rss")
+      @feed_title = feed.channel.title
+      @title = item.title
+      @link = item.link
+      @date = parse_time(item.date)
+      @content = item.description
+      @content = item.content_encoded if item.content_encoded
+      @guid = item.guid.content
+      @audio = nil
+      if item.enclosure
+        if item.enclosure.type == "audio/mpeg"
+          @audio = item.enclosure.url
+        elsif item.enclosure.type.start_with? "image/" # If there is an image in the enclosure
+          unless entry[ENTRY_CONTENT].include? "<img " # and no images in the content
+            # then include the enclosure image
+            @content = "<img src=\"#{item.enclosure.url}\" /><br/>" + @content
+          end
+        end
+      end
+    elsif (feed.feed_type == "atom")
+      @feed_title = feed.title.content
+      @title = item.title.content
+      @link = item.link.href
+      if !item.published.nil?
+        @date = parse_time(item.published.content)
+      else
+        @date = parse_time(item.updated.content)
+      end
+      if !item.content.nil?
+        @content = CGI.unescapeHTML(item.content.to_s)
+      else
+        @content = CGI.unescapeHTML(item.summary.to_s)
+      end
+      @guid = item.id.to_s
+      @audio = nil # TODO podcast support for Atom feeds?
+    end
+  end
+
+  # returns array of HavenFeedEntry objects
+  # feed_url is the URL of a feed, eg: "https://example.com/rss.xml"
+  def self.fetch_feed_content(feed_url)
+    entries = nil
+    cleanurl, auth_opts = parse_auth(feed_url)
+    URI.open(cleanurl, auth_opts) do |rss|
+      entries = parse_feed_content(rss)
+    end
+    entries
+  end
+
+  # returns array of HavenFeedEntry objects
+  # feed_raw is a StringIO from URI.open (or a File for testing)
+  def self.parse_feed_content(feed_raw)
+      entries=[]
+      feed = RSS::Parser.parse(feed_raw, validate: false)
+      if (feed.feed_type == "rss")
+        feed.items.each do |item|
+          entry = HavenFeedEntry.new(feed,item)
+          entries << entry
+        end
+      elsif (feed.feed_type == "atom")
+        feed.entries.each do |item|
+          entry = HavenFeedEntry.new(feed,item)
+          entries << entry
+        end
+      end
+    entries
+  end
+
+  def self.parse_auth(full_url)
+    scheme, rest = full_url.split("://",2)
+    opts = {}
+    opts["User-Agent"] = "haven"
+    if (rest.include?(":") and rest.include?("@")) # scheme://user:pass@url...
+      user, rest = rest.split(":",2)
+      pass, rest = rest.split("@",2)
+      opts[:http_basic_authentication] = [user,pass]
+      return ["#{scheme}://#{rest}", opts]
+    else
+      return [full_url, opts]
+    end
+  end
+
+  private
+
+  # Different time formats were causing problems, this method standardizes them
+  def parse_time(time)
+    if time.is_a? Time
+      return Time.parse time.httpdate
+    elsif time.is_a String
+      return Time.parse(Time.parse(time).httpdate)
+    else
+      raise "Argument Error, #{time} is not a valid time"
+    end
+  end
+
+end
+
---
+++ test/models/feed_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+require File.join(Rails.root, "lib","haven_feed_entry.rb") 
+
+class FeedTest < ActiveSupport::TestCase
+  test "should allow new feed entries to be saved to a feed" do
+    user = User.find(1)
+    (1..10).each do |i|
+      feed = user.feeds.create!({url:"https://feed#{i}.url/rss"})
+      feed.with_lock do
+        (1..10).each do |j|
+          title = "Post Title #{j}"
+          link = "https://feed#{i}.url/#{j}.html"
+          published = Time.zone.now
+          sort_date = published
+          content = "My Post Number #{j}"
+          audio = nil
+          guid = link
+          record_data = {title: title, link: link, published: published, sort_date: sort_date, content: content, audio: audio, guid: guid}
+          entry = feed.feed_entries.create!(record_data)
+          assert_not_nil entry.published
+        end
+      end # release lock
+    end
+  rescue => e
+    assert false, "Error saving feed: #{e}"
+  end
+
+  test "should_accept_feed_entries_from_an_rss_feed" do
+    user = User.find(1)
+    feed = user.feeds.create!({url:"https://havenweb.org/feed.xml"})
+    atom_file = File.open(File.join(Rails.root,"test","fixtures","files","haven-rss.xml"))
+    entries = HavenFeedEntry.parse_feed_content(atom_file)
+    entries.each do |e|
+      record_data = {title: e.title, link: e.link, published: e.date, sort_date: e.date, content: e.content, audio: e.audio, guid: e.guid}
+      table_entry = feed.feed_entries.create!(record_data)
+      assert_not_nil table_entry.published
+    end
+  end
+
+  test "should_accept_feed_entries_from_an_atom_feed" do
+    user = User.find(1)
+    feed = user.feeds.create!({url:"https://xkcd.com/atom.xml"})
+    atom_file = File.open(File.join(Rails.root,"test","fixtures","files","xkcd-atom.xml"))
+    entries = HavenFeedEntry.parse_feed_content(atom_file)
+    entries.each do |e|
+      record_data = {title: e.title, link: e.link, published: e.date, sort_date: e.date, content: e.content, audio: e.audio, guid: e.guid}
+      table_entry = feed.feed_entries.create!(record_data)
+      assert_not_nil table_entry.published
+    end
+  end
+end
