3d1db6c0b9770ab5dce17d989f68abe87ba18fca
Matthew Wise <matt@havenweb.org>
2023-04-29 21:35:30 -0500

    finalize upgrade to rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
