81b54a1425892bcae4543e8772c8f90a506609b8
Matt <matt@havenweb.org>
2024-10-16 09:00:36 -0500

    allow editing the data and time of a post after it has been published

--- test/system/editing_posts_test.rb
+++ test/system/editing_posts_test.rb
@@ -53,6 +53,35 @@ class EditingPostsTest < ApplicationSystemTestCase
       click_on "Save Post"
       assert_text m3
     click_on "Logout"
+  end
 
+  test "can_edit_post_date_but_not_visible_on_post_creation" do
+    log_in_with test_users[:jackson] #publisher
+    click_on "New Post Button"
+
+    ## ensure we cannot edit the date and time of the post
+    assert_no_selector 'input[id="post_date"][type="date"]'
+    assert_no_selector 'input[id="post_time"][type="time"]'
+
+    m = "#{rand} I cannot tell a lie"
+    fill_in "post_content", with: m
+    click_on "Save Post"
+    assert_text m # page previw shows post content
+
+    click_on "Edit"
+    ## ensure we can edit the date and time of the post
+    assert_selector 'input[id="post_date"][type="date"]'
+    assert_selector 'input[id="post_time"][type="time"]'
+
+    ## edit the date/time and see it persist
+    fill_in "post_date", with: "01022023"
+    fill_in "post_time", with: "0123p"
+    click_on "Save Post"
+    assert_text "January 02, 2023"
+    click_on "Edit"
+    assert_selector 'input[id="post_date"][type="date"][value="2023-01-02"]'
+    assert_selector 'input[id="post_time"][type="time"][value="13:23"]'
+ 
+    click_on "Logout"
   end
 end
