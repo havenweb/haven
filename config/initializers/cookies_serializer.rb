6aa1fc690adbd95524d497cc3cc8c6e92365f3df
Matt <matt@havenweb.org>
2025-03-17 13:51:02 -0500

    Finalize rails 7.0 config defaults

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -2,4 +2,15 @@
 
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
-Rails.application.config.action_dispatch.cookies_serializer = :json
+
+# If you're upgrading and haven't set `cookies_serializer` previously, your cookie serializer
+# is `:marshal`. The default for new apps is `:json`.
+#
+# To migrate an existing application to the `:json` serializer, use the `:hybrid` option.
+#
+# Rails transparently deserializes existing (Marshal-serialized) cookies on read and
+# re-writes them in the JSON format.
+#
+# It is fine to use `:hybrid` long term; you should do that until you're confident *all* your cookies
+# have been converted to JSON.
+Rails.application.config.action_dispatch.cookies_serializer = :hybrid
