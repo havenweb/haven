c54d5c7d58315e2f6e1b45f09cca3d1074d34a21
Matt <matt@havenweb.org>
2025-04-26 12:49:16 -0500

    allow demo users to see markdown instructions

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,8 @@
 class StaticController < ApplicationController
   before_action :authenticate_user!
-  before_action :verify_publisher
-  before_action :verify_admin, except:[:themes]
+## Demo should expose these endpoints to all logged-in users
+#  before_action :verify_publisher
+#  before_action :verify_admin, except:[:themes]
   def markdown
   end
   def themes
