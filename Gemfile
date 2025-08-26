737dc09d7e25ea5558183b3dd56e97c7b4d0a339
Matt <matt@havenweb.org>
2025-08-26 10:56:26 -0500

    upgrade to rails 7.1.5.2

--- Gemfile
+++ Gemfile
@@ -30,7 +30,7 @@ gem 'bcrypt', '~> 3.1.16'
 gem 'kaminari', '~> 1.2.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 7.0.8.7'
+gem 'rails', '~> 7.1.5.2'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
 gem 'pg', '~> 1.5.9'
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,11 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -30,9 +35,5 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    ## from migration to Rails version 7.0.x
-    #config.active_support.cache_format_version = 7.0
-    #config.active_support.disable_to_s_conversion = true
   end
 end
