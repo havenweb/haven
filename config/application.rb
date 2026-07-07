4560c14b4d9d5ba641a43e178f0de357a5da6664
Matt <matt@havenweb.org>
2026-07-07 17:15:47 -0500

    bump to rails 7.2

--- config/application.rb
+++ config/application.rb
@@ -21,16 +21,12 @@ Bundler.require(*Rails.groups)
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
-
-    config.add_autoload_paths_to_load_path = false
-
-    config.active_support.cache_format_version = 7.1
-
+    config.load_defaults 7.2
+    
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
